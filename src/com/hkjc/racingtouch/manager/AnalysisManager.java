fdf0984cfc662c55613c88a212ce555a7f0ff862
z1147 <z1147@MGEMIP90043.mpel.com>
2018-02-13 16:03:40 +0800

    corrected

--- src/com/hkjc/racingtouch/manager/AnalysisManager.java
+++ src/com/hkjc/racingtouch/manager/AnalysisManager.java
@@ -17,14 +17,14 @@ public class AnalysisManager {
 	}
 	
 	public List<Map<String, Object>> getResultRecordsByGroup(List<String> groupByCols){
-		String queryString = "SELECT " + groupByCols.toString() + ", COUNT(*) as total FROM [HKJC].[dbo].RaceHorse rh INNER JOIN RaceResult rr ON rh.MeetingDate = rr.MeetingDate "
+		String queryString = "SELECT " + groupByCols.toString() + ", COUNT(position) as winTotal, COUNT(*) as total, (winTotal/total)*10 as winPercentage FROM [HKJC].[dbo].RaceHorse rh LEFT OUTER JOIN RaceResult rr ON rh.MeetingDate = rr.MeetingDate "
 				+ "AND rh.RaceNo = rr.RaceNo GROUP BY " + groupByCols.toString() + " ORDER BY total desc";
 		
 		return executeQuery(queryString, groupByCols);
 	}
 	
 	public List<Map<String, Object>> getResultDividendsByGroup(List<String> groupByCols){
-		String queryString = "SELECT " + groupByCols.toString() + ", COUNT(*) as total FROM [HKJC].[dbo].RaceHorse rh INNER JOIN RaceResult rr ON rh.MeetingDate = rr.MeetingDate "
+		String queryString = "SELECT " + groupByCols.toString() + ", COUNT(position) as winTotal, COUNT(*) as total, (winTotal/total)*10*dividendAmount as winPercentage FROM [HKJC].[dbo].RaceHorse rh LEFT OUTER JOIN RaceResult rr ON rh.MeetingDate = rr.MeetingDate "
 				+ "AND rh.RaceNo = rr.RaceNo INNER JOIN Dividend d ON rh.MeetingDate = d.MeetingDate AND rh.RaceNo = d.RaceNo GROUP BY " + groupByCols.toString() + " ORDER BY total desc";
 		
 		return executeQuery(queryString, groupByCols);
@@ -32,7 +32,7 @@ public class AnalysisManager {
 	
 	private List<Map<String, Object>> executeQuery(String sqlQuery, List<String> columns){
 		Connection conn = new SQLUtil().getConnection();
-		
+		System.out.println("SQL Query: " + sqlQuery);
 		try {
 			ResultSet resultSet = conn.createStatement().executeQuery( sqlQuery );
 			
