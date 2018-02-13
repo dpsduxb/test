f4eea37f2787107fbd5219c119a30f53b03f11e1
z1147 <z1147@MGEMIP90043.mpel.com>
2018-02-13 16:11:47 +0800

    position added

--- src/com/hkjc/racingtouch/manager/AnalysisManager.java
+++ src/com/hkjc/racingtouch/manager/AnalysisManager.java
@@ -11,21 +11,27 @@ import com.hkjc.racingtouch.utils.SQLUtil;
 public class AnalysisManager {
 
 	public List<Map<String, Object>> getRecordsByGroup(List<String> groupByCols){
-		String queryString = "SELECT " + groupByCols.toString() + ", COUNT(*) as total FROM [HKJC].[dbo].RaceHorse GROUP BY " + groupByCols.toString() + " ORDER BY total desc";;
+		String queryString = "SELECT " + groupByCols.toString() + ", COUNT(*) as total FROM [HKJC].[dbo].RaceHorse GROUP BY " 
+								+ groupByCols.toString() + " ORDER BY total desc";;
 		
 		return executeQuery(queryString, groupByCols);
 	}
 	
 	public List<Map<String, Object>> getResultRecordsByGroup(List<String> groupByCols){
-		String queryString = "SELECT " + groupByCols.toString() + ", COUNT(position) as winTotal, COUNT(*) as total, (winTotal/total)*10 as winPercentage FROM [HKJC].[dbo].RaceHorse rh LEFT OUTER JOIN RaceResult rr ON rh.MeetingDate = rr.MeetingDate "
-				+ "AND rh.RaceNo = rr.RaceNo GROUP BY " + groupByCols.toString() + " ORDER BY total desc";
+		String queryString = "SELECT " + groupByCols.toString() + ", COUNT(position) as winTotal, COUNT(*) as total, "
+				+ "(winTotal/total)*10 as winPercentage FROM [HKJC].[dbo].RaceHorse rh LEFT OUTER JOIN RaceResult rr "
+				+ "ON rh.MeetingDate = rr.MeetingDate AND rh.RaceNo = rr.RaceNo AND rh.HorseNo = rr.HorseNo WHERE position =1 GROUP BY " 
+				+ groupByCols.toString() + " ORDER BY total desc";
 		
 		return executeQuery(queryString, groupByCols);
 	}
 	
 	public List<Map<String, Object>> getResultDividendsByGroup(List<String> groupByCols){
-		String queryString = "SELECT " + groupByCols.toString() + ", COUNT(position) as winTotal, COUNT(*) as total, (winTotal/total)*10*dividendAmount as winPercentage FROM [HKJC].[dbo].RaceHorse rh LEFT OUTER JOIN RaceResult rr ON rh.MeetingDate = rr.MeetingDate "
-				+ "AND rh.RaceNo = rr.RaceNo INNER JOIN Dividend d ON rh.MeetingDate = d.MeetingDate AND rh.RaceNo = d.RaceNo GROUP BY " + groupByCols.toString() + " ORDER BY total desc";
+		String queryString = "SELECT " + groupByCols.toString() + ", COUNT(position) as winTotal, COUNT(*) as total, "
+				+ "(winTotal/total)*10*dividendAmount as winPercentage FROM [HKJC].[dbo].RaceHorse rh LEFT OUTER JOIN RaceResult rr "
+				+ "ON rh.MeetingDate = rr.MeetingDate "
+				+ "AND rh.RaceNo = rr.RaceNo INNER JOIN Dividend d ON rh.MeetingDate = d.MeetingDate AND rh.RaceNo = d.RaceNo GROUP BY " 
+				+ groupByCols.toString() + " ORDER BY total desc";
 		
 		return executeQuery(queryString, groupByCols);
 	}
