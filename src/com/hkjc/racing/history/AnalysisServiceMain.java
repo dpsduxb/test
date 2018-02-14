1a3ad52e866da4c57be31dcd9cf1d977a72aee55
adi369 <umamaheswara.bandi@tcs.com>
2018-02-14 06:07:53 +0530

    changes

--- src/com/hkjc/racing/history/AnalysisServiceMain.java
+++ src/com/hkjc/racing/history/AnalysisServiceMain.java
@@ -13,12 +13,14 @@ public class AnalysisServiceMain {
 		List<List<String>> combinations = new Combinations().getCombinations();
 		for (Iterator iterator = combinations.iterator(); iterator.hasNext();) {
 			List<String> columns = (List<String>) iterator.next();
-			
-			AnalysisManager analysisManager = new AnalysisManager();
-			List<Map<String, Object>> recordsByGroup = analysisManager.getRecordsByGroup(columns);
-			
-			List<Map<String, Object>> resultRecordsByGroup = analysisManager.getResultRecordsByGroup(columns);
-			
+			if(columns != null && columns.size() > 0){
+				AnalysisManager analysisManager = new AnalysisManager();
+				String columnsStr = columns.toString().substring(1,columns.toString().length()-1);
+				//String columnsStr = columns.toString().replaceAll("[", "").replaceAll("]", "");
+				List<Map<String, Object>> recordsByGroup = analysisManager.getRecordsByGroup(columnsStr);
+				
+				List<Map<String, Object>> resultRecordsByGroup = analysisManager.getResultRecordsByGroup(columnsStr);
+			}
 		}
 	}
 
--- src/com/hkjc/racingtouch/manager/AnalysisManager.java
+++ src/com/hkjc/racingtouch/manager/AnalysisManager.java
@@ -10,14 +10,14 @@ import com.hkjc.racingtouch.utils.SQLUtil;
 
 public class AnalysisManager {
 
-	public List<Map<String, Object>> getRecordsByGroup(List<String> groupByCols){
-		String queryString = "SELECT " + groupByCols.toString() + ", COUNT(*) as total FROM [HKJC].[dbo].RaceHorse GROUP BY " 
-								+ groupByCols.toString() + " ORDER BY total desc";;
+	public List<Map<String, Object>> getRecordsByGroup(String groupByCols){
+		String queryString = "SELECT " + groupByCols + ", COUNT(*) as total FROM [HKJC].[dbo].RaceHorse GROUP BY " 
+								+ groupByCols + " ORDER BY total desc";;
 		
 		return executeQuery(queryString, groupByCols);
 	}
 	
-	public List<Map<String, Object>> getResultRecordsByGroup(List<String> groupByCols ){
+	public List<Map<String, Object>> getResultRecordsByGroup(String groupByCols ){
 		String queryString = "SELECT " + groupByCols.toString() + ", COUNT(position) as winTotal, COUNT(*) as total, "
 				+ "(winTotal/total)*10 as winPercentage FROM [HKJC].[dbo].RaceHorse rh LEFT OUTER JOIN RaceResult rr "
 				+ "ON rh.MeetingDate = rr.MeetingDate AND rh.RaceNo = rr.RaceNo AND rh.HorseNo = rr.HorseNo WHERE position = 1 GROUP BY " 
@@ -26,9 +26,9 @@ public class AnalysisManager {
 		return executeQuery(queryString, groupByCols);
 	}
 	
-	public List<Map<String, Object>> getResultDividendsByGroup(List<String> groupByCols){
+	public List<Map<String, Object>> getResultDividendsByGroup(String groupByCols){
 		String queryString = "SELECT " + groupByCols.toString() + ", COUNT(position) as winTotal, COUNT(*) as total, "
-				+ "(winTotal/total)*10*dividendAmount as winPercentage FROM [HKJC].[dbo].RaceHorse rh LEFT OUTER JOIN RaceResult rr "
+				+ "(winTotal/total)*10*dividendAmount as winPercentage FROM [HKJC].[dbo].RaceHorse rh LEFT OUTER JOIN [HKJC].[dbo].RaceResult rr "
 				+ "ON rh.MeetingDate = rr.MeetingDate "
 				+ "AND rh.RaceNo = rr.RaceNo INNER JOIN Dividend d ON rh.MeetingDate = d.MeetingDate AND rh.RaceNo = d.RaceNo GROUP BY " 
 				+ groupByCols.toString() + " ORDER BY total desc";
@@ -36,7 +36,7 @@ public class AnalysisManager {
 		return executeQuery(queryString, groupByCols);
 	}
 	
-	private List<Map<String, Object>> executeQuery(String sqlQuery, List<String> columns){
+	private List<Map<String, Object>> executeQuery(String sqlQuery, String columns){
 		Connection conn = new SQLUtil().getConnection();
 		System.out.println("SQL Query: " + sqlQuery);
 		try {
